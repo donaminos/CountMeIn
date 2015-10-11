a5c3cd0c857f941bbaa722be061df409525bed41
= <=>
2015-10-11 18:33:00 +0530

    chat

--- client/controllers/EventChatCtrl.js
+++ client/controllers/EventChatCtrl.js
@@ -1,9 +1,35 @@
 angular.module('countmein').controller('EventChatCtrl', ['$scope', '$meteor', '$stateParams',
     function($scope, $meteor, $stateParams){
-            
-        console.log("Discussion:" + $stateParams.eventId);
+        $scope.$meteorSubscribe('events'); 
+        $scope.chat = {};
+		$scope.chat.text;
+
+        var eventid = $stateParams.eventId;
+        var currentuserId = "acdfkdfdxdfdf";
         
         // access parent scope
-        console.log("Parent:" + $scope.$parent.currentEvent.name);
+        //console.log("Parent:" + $scope.$parent.currentEvent);
         
-    }]);
+        $scope.showChat = $meteor.collection(function(){
+            return Events.find({ 
+            	participants : { 
+            		$elemMatch: {
+                     userId: currentuserId
+                	} 
+            	}, _id: eventid 
+            });
+        });
+
+        $scope.chatnow = function(){
+        	if($scope.chat.text != ""){
+        		 $meteor.call("addChatItem", eventid ,{
+			         text : $scope.chat.text,
+			         image : "", 
+			         important : 1, 
+			         votes : 0 ,
+			         userId : currentuserId
+		         }); 
+        	}
+        }
+
+}]);
--- model/events.js
+++ model/events.js
@@ -10,6 +10,20 @@ Meteor.methods({
     updateEvent:function(currentEvent)
     {
         Events.update({_id:currentEvent._id}, currentEvent);
+    },
+
+   	addChatItem : function(id,chat){
+        /*
+         chat ==>
+         {
+	         text : "",
+	         image : "", --> "" or url
+	         important :  --> 1-10
+	         votes : 10 --> numerical,number of upvotse 
+	         userId : ""
+         }
+        */
+        Events.update({ _id:id}, { $push: { chat : chat} })
     }
     
 });
